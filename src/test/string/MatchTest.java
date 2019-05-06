db908f7d6c20427769416b26dd47b42dd777710e
qbging <329904771@qq.com>
2019-05-06 15:22:30 +0800

    括号正对 匹配测试

---
+++ src/test/string/MatchTest.java
@@ -0,0 +1,30 @@
+package test.string;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchTest {
+
+    public static void main(String[] args) {
+        String str ="中华人民共和国，简称（中国）。";
+            Matcher mat = Pattern.compile("(?<=\\（)(\\S+)(?=\\）)").matcher(str);//此处是中文输入的（）
+            while(mat.find()){
+                     System.out.println(mat.group());
+                 }
+        String string = "baabrraaab";
+        Pattern pattern = Pattern.compile("a*b");
+        Matcher matcher = pattern.matcher(string);
+        //matches是整串匹配，find是部分匹配
+        System.out.println("matches："+matcher.matches());//string是aaab时true，raaab时false
+//        System.out.println("find:"+matcher.find());//string是aaab时false，raaab时true
+        while (matcher.find()){//此处之前如果调用了find，则打印的是开始第二次匹配的子序列
+            System.out.println("匹配的子序列："+matcher.group());
+        }
+
+        /*输出：中国
+        matches：false
+        匹配的子序列：b///如果上面注释部分放开，则这行输出就没有了
+        匹配的子序列：aab
+        匹配的子序列：aaab*/
+    }
+}
